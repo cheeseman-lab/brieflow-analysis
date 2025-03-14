31e9b879f9206736d86c840408126474b4c9b459
Roshan Kern <rsk119@case.edu>
2025-03-14 11:56:42 -0400

    update cell class

--- analysis/8.run_aggregate.sh
+++ analysis/8.run_aggregate.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# Log all output to a log file (stdout and stderr)
+mkdir -p slurm/slurm_output/main
+start_time_formatted=$(date +%Y%m%d_%H%M%S)
+log_file="slurm/slurm_output/test/merge-${start_time_formatted}.log"
+exec > >(tee -a "$log_file") 2>&1
+
+echo "Started at: $(date)"
+
 # Run the merge process rules
 snakemake --use-conda --cores all \
     --snakefile "../brieflow/workflow/Snakefile" \
     --configfile "config/config.yml" \
     --rerun-triggers mtime \
-    --until all_aggregate -n
+    --until all_aggregate
+
+echo "Ended at: $(date)"
