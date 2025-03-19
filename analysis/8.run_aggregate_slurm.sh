76a7477b570c0492ca735ecc925ec2091c98a0bf
Roshan Kern <rsk119@case.edu>
2025-03-19 08:22:16 -0400

    update final run reqs

--- analysis/8.run_aggregate_slurm.sh
+++ analysis/8.run_aggregate_slurm.sh
@@ -3,14 +3,14 @@
 # Log all output to a log file (stdout and stderr)
 mkdir -p slurm/slurm_output/main
 start_time_formatted=$(date +%Y%m%d_%H%M%S)
-log_file="slurm/slurm_output/test/merge-${start_time_formatted}.log"
+log_file="slurm/slurm_output/main/aggregate-${start_time_formatted}.log"
 exec > >(tee -a "$log_file") 2>&1
 
 echo "Started at: $(date)"
 
 # Activate conda environment (adjust path as needed)
 source ~/.bashrc
-conda activate brieflow_workflows
+conda activate brieflow_agg_overhaul_env
 
 # Run the aggregate rules
 snakemake --executor slurm --use-conda \
@@ -19,6 +19,7 @@ snakemake --executor slurm --use-conda \
     --configfile "config/config.yml" \
     --latency-wait 60 \
     --rerun-triggers mtime \
-    --until all_aggregate
+    --keep-going \
+    --until eval_aggregate
 
 echo "Ended at: $(date)"
