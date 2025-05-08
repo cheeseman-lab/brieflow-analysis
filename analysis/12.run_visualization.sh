6be440dad7e883f2e6d45b255d34f39037e1e141
Roshan Kern <rsk119@case.edu>
2025-05-08 16:52:02 -0400

    rename to brieflow output

--- analysis/12.run_visualization.sh
+++ analysis/12.run_visualization.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export BRIEFLOW_OUTPUT_PATH="/lab/cheeseman_ops/brieflow-screens/aconcagua-analysis/analysis/analysis_root"
-export CONFIG_PATH="/lab/cheeseman_ops/brieflow-screens/aconcagua-analysis/analysis/config/config.yml"
-export SCREEN_PATH="/lab/cheeseman_ops/brieflow-screens/aconcagua-analysis/analysis/screen.yaml"
+export BRIEFLOW_OUTPUT_PATH="brieflow_output/"
+export CONFIG_PATH="config/config.yml"
+export SCREEN_PATH="screen.yaml"
 
 # Start Streamlit server, force bind to 0.0.0.0
 exec streamlit run ../brieflow/visualization/Experimental_Overview.py --server.address=0.0.0.0 "$@"
