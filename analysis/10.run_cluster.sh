b5e192909339591fc65ddb2098ecdd9f54767992
mdiberna <mdiberna@fry.wi.mit.edu>
2025-12-04 13:52:22 -0500

    rename noteboosk and files according with new module

--- analysis/10.run_cluster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run cluster rules
-snakemake --use-conda --cores all \
-    --snakefile "../brieflow/workflow/Snakefile" \
-    --configfile "config/config.yml" \
-    --rerun-triggers mtime \
-    --keep-going \
-    --until all_cluster -n
--- analysis/12.run_visualization.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export BRIEFLOW_OUTPUT_PATH="brieflow_output/"
-export CONFIG_PATH="config/config.yml"
-export SCREEN_PATH="screen.yaml"
-
-# Start Streamlit server, force bind to 0.0.0.0
-exec streamlit run ../brieflow/visualization/Experimental_Overview.py --server.address=0.0.0.0 "$@"
